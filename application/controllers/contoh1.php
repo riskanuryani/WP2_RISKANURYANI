ec6a7e7073f464170e2242bddab8d522299f0418
riskanuryani <riskanuryani87@gmail.com>
2021-10-06 20:51:57 +0700

    Upload awal project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Riska Nuryani tinggal di Pontianak
+            olahraga yang disukai adalah Bulu Tangkis";
+    }
+}
